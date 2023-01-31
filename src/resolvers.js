705bbe882ae5359d2de3be9142c59c75bded4369
justBadProgrammer <102511582+justBadProgrammer@users.noreply.github.com>
2023-01-31 19:11:25 +0700

    schema changed

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,21 +1,18 @@
 const resolvers = {
-  Query: {
+  Query: 
+    {
     // returns an array of Tracks that will be used to populate the homepage grid of our web client
-    tracksForHome: (_, __, { dataSources }) => {
-      return dataSources.trackAPI.getTracksForHome();
-    },
+    tracksForHome: (_parent, _args, { dataSources }, _info) =>
+      dataSources.trackAPI.getTracksForHome(),
 
     // get a single track by ID, for the track page
-    track: (_, { id }, { dataSources }) => {
-      return dataSources.trackAPI.getTrack(id);
-    },
+    track: (_parent, { id }, { dataSources }, _info) => dataSources.trackAPI.getTrack(id),
 
     // get a single module by ID, for the module detail page
-    module: (_, { id }, { dataSources }) => {
-      return dataSources.trackAPI.getModule(id);
-    },
+    module: (_parent, { id }, { dataSources },_info) => dataSources.trackAPI.getModule(id)
   },
-  Mutation: {
+  Mutation: 
+    {
     // increments a track's numberOfViews property
     incrementTrackViews: async (_, { id }, { dataSources }) => {
       try {
@@ -36,15 +33,19 @@ const resolvers = {
       }
     },
   },
-  Track: {
-    author: ({ authorId }, _, { dataSources }) => {
-      return dataSources.trackAPI.getAuthor(authorId);
+  Track: 
+    {
+      author: ({ authorId }, _args, { dataSources }, _info) => 
+        dataSources.trackAPI.getAuthor(authorId),
+      modules: ({ id }, _args, { dataSources }, _info) => 
+        dataSources.trackAPI.getTrackModules(id),
+      durationInSeconds: ({length}, _args, _context, _info) => length,  
     },
-
-    modules: ({ id }, _, { dataSources }) => {
-      return dataSources.trackAPI.getTrackModules(id);
-    },
-  },
+  Module:
+    {
+      durationInSeconds: ({length}, _args, _context, _info) => length,
+      track: ({trackId}, _args, { dataSources }, _info) => dataSources.trackAPI.getTrack(trackId),
+    }  
 };
 
 module.exports = resolvers;
